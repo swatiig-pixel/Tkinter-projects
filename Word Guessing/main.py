bf26b7d3756bd4ab5d562b9472ac65231dbcc4d7
Swati <swatiig1903@gmail.com>
2025-10-11 21:14:08 +0530

    Levels like Easy ,Medium and Hard are added in word meaning project

--- Word Guessing/main.py
+++ Word Guessing/main.py
@@ -4,10 +4,24 @@ import random
 import time
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pd.read_csv("C:/tkinter projects/Word Guessing/CSV files/Kannada_to_English_Easy.csv")
-to_learn = data.to_dict(orient="records")
-current_slide = {}
+def choose_level():
+  level = Tk()
+  level.title("Choose Level")
+  level.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
+  Label(level, text="Select Level", font=("Arial", 20, "bold"), bg=BACKGROUND_COLOR).pack(pady=20)
+
+  Button(level, text="Easy", font=("Arial", 15), width=15, command=lambda: select(level,"Easy")).pack(pady=5)
+  Button(level, text="Medium", font=("Arial", 15), width=15, command=lambda: select(level,"Medium")).pack(pady=5)
+  Button(level, text="Hard", font=("Arial", 15), width=15, command=lambda: select(level,"Hard")).pack(pady=5)
+
+  level.mainloop()
+
+def select(window_to_close,level):
+  window_to_close.destroy()
+  start_game(level)
+
+  
 
 def on_correct_click():
   global current_slide, flip_timer
@@ -29,35 +43,48 @@ def is_known():
   to_learn.remove(current_slide)
   on_correct_click()
 
+def start_game(level):
+  global window, data, to_learn, flip_timer, canvas, main_k_word, k_heading, bg_image, front_card, back_card
 
+    # Pick CSV file based on level
+  if level == "Easy":
+    path = "C:/tkinter projects/Word Guessing/CSV files/Kannada_to_English_Easy.csv"
+  elif level == "Medium":
+    path = "C:/tkinter projects/Word Guessing/CSV files/Kannada_to_English_Medium.csv"
+  else:
+    path = "C:/tkinter projects/Word Guessing/CSV files/Kannada_to_English_Hard.csv"
 
-window = Tk()
-window.title("Word Guess")
-window.config(width=800,height=526,padx=50,pady=50,bg=BACKGROUND_COLOR)
+    # Load data
+  data = pd.read_csv(path)
+  to_learn = data.to_dict(orient="records")
 
-flip_timer = window.after(3000,flipped_card)
+  window = Tk()
+  window.title(f"Word Guess- {level} level ")
+  window.config(width=800,height=526,padx=50,pady=50,bg=BACKGROUND_COLOR)
 
-#card image
-canvas = Canvas(window,width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
-canvas.grid(column=0,row=0,columnspan=2)
-front_card = PhotoImage(file="C:/tkinter projects/Word Guessing/images/card_front.png")
-back_card = PhotoImage(file="C:/tkinter projects/Word Guessing/images/card_back.png")
-bg_image = canvas.create_image(400,263,image=front_card)
-k_heading = canvas.create_text(400,150,text="Title",font=("Arial",40,"normal"))
-main_k_word = canvas.create_text(400,263,text="Word",font=("Arial",60,"bold"))
+  flip_timer = window.after(3000,flipped_card)
 
-#Correct button
-correct_image = PhotoImage(file="C:/tkinter projects/Word Guessing/images/right.png")
-correct_button = Button(image=correct_image,highlightthickness=0,command=is_known)
-correct_button.grid(column=1,row=1)
+  #card image
+  canvas = Canvas(window,width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
+  canvas.grid(column=0,row=0,columnspan=2)
+  front_card = PhotoImage(file="C:/tkinter projects/Word Guessing/images/card_front.png")
+  back_card = PhotoImage(file="C:/tkinter projects/Word Guessing/images/card_back.png")
+  bg_image = canvas.create_image(400,263,image=front_card)
+  k_heading = canvas.create_text(400,150,text="Title",font=("Arial",40,"normal"))
+  main_k_word = canvas.create_text(400,263,text="Word",font=("Arial",60,"bold"))
 
-#Wrong button
-wrong_image = PhotoImage(file="C:/tkinter projects/Word Guessing/images/wrong.png",)
-wrong_button = Button(image=wrong_image,highlightthickness=0,command=on_correct_click)
-wrong_button.grid(column=0,row=1)
+  #Correct button
+  correct_image = PhotoImage(file="C:/tkinter projects/Word Guessing/images/right.png")
+  correct_button = Button(image=correct_image,highlightthickness=0,command=is_known)
+  correct_button.grid(column=1,row=1)
 
-on_correct_click()
+  #Wrong button
+  wrong_image = PhotoImage(file="C:/tkinter projects/Word Guessing/images/wrong.png",)
+  wrong_button = Button(image=wrong_image,highlightthickness=0,command=on_correct_click)
+  wrong_button.grid(column=0,row=1)
 
+  on_correct_click()
 
+  window.mainloop()
 
-window.mainloop()
+choose_level()
