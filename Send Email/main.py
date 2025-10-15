ec7e1621cc5b880b6f8fd042eb9a7314beb7799e
Swati <swatiig1903@gmail.com>
2025-10-15 23:50:39 +0530

    Mail sent with quote

--- Send Email/main.py
+++ Send Email/main.py
@@ -1,8 +1,25 @@
 import smtplib
 
-my_email = "arcane.lifestyle09.gmail.com"
-password = "gfhm sqmm yvyd vetq"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email,password=password)
-connection.sendmail(from_addr=my_email,to_addrs="swatiig1903@gmail.com",msg="Hello")
+import random
+import datetime as dt
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 1:
+  with open(file="C:/tkinter projects/Send Email/quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = random.choice(all_quotes)
+
+
+
+my_email = "sender_mail.com"
+password = "app_password"
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465) as connection:
+  connection.login(user=my_email,password=password)
+  connection.sendmail(
+    from_addr=my_email,
+    to_addrs="reciever_mail@gmail.com",
+    msg=quote"
+   )
